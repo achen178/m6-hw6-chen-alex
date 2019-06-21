32e2aea2b11a92791f9cd8a3eceddab9a4f2f9d7
Alex Chen <32882075+achen178@users.noreply.github.com>
2019-06-20 22:21:52 -0400

    Setup click event and transition for button

---
+++ main.js
@@ -0,0 +1,30 @@
+var button = document.getElementsByTagName('button')[0];
+var myHeading = document.getElementsByTagName('h1')[0];
+var result = document.getElementById('result');
+var resturantResult = document.getElementById('random-result')
+
+
+var restaurantList = [
+  'The Salty Pig',
+  'Parish Cafe and Bar',
+  'Milkweed',
+  'Carmelina’s',
+  'Saltie Girl',
+  'Bostonia Public House',
+  'The Friendly Toast',
+  'Mike & Patty’s',
+  'The Tip Tap Room',
+  'Toro',
+  'Blunch',
+  'Citrus & Salt',
+  'Dig Inn',
+  'Row 34',
+  'Luke’s Lobster Back Bay',
+];
+
+var myFunction = function(){
+  resturantResult.innerHTML = restaurantList[0];
+  result.classList.toggle('hidden');
+};
+
+button.onclick = myFunction;
